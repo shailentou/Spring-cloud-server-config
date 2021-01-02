2206959c0774ceb7f9fc0056827edcb3854d0109
shailentou <32923113+shailentou@users.noreply.github.com>
2021-01-02 07:37:58 +0530

    initial commit

---
+++ Config-Server/confsvr/src/main/java/com/shail/config/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.shail.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+	
+	public static void main(String[] args) {
+		System.out.println("FSDFSFS");
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
