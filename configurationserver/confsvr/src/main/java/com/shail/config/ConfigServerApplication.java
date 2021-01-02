4e879377caa4b6b81cc37bbf5a224daf5e8b4d3a
shailentou <shailentou@gmail.com>
2021-01-02 09:46:57 +0530

    initial commit

--- configurationserver/confsvr/src/main/java/com/shail/config/ConfigServerApplication.java
+++ configurationserver/confsvr/src/main/java/com/shail/config/ConfigServerApplication.java
@@ -11,6 +11,7 @@ public class ConfigServerApplication {
 	public static void main(String[] args) {
 		System.out.println("FSDFSFS");
 		SpringApplication.run(ConfigServerApplication.class, args);
+		
 	}
 
 }
